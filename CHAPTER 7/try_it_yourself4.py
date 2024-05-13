27968ee85614510e485d2be7f86d25aef58baada
fysalkasim <fysalkasim@gmail.com>
2024-05-13 13:26:30 +0530

    Pizza

--- CHAPTER 7/try_it_yourself4.py
+++ CHAPTER 7/try_it_yourself4.py
@@ -1,3 +1,8 @@
-x = 1
-while x <= 5:
- print(x)
+prompt = "\nTell me your pizza topping:"
+prompt += "\nEnter 'quit' to end the program. "
+while True:
+    toopings = input(prompt)
+    if toopings == 'quit':
+        break
+    else:
+        print(f"Adding the {toopings} to your pizza")
